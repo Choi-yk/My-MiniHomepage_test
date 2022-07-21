9bb8233837523ca835e9ce792b7343b20f766fdb
choiyk <weareyk@gmail.com>
2022-07-21 16:10:50 +0900

    diary commit

--- minipage/src/main/java/com/mysite/minipage/diary/DiaryController.java
+++ minipage/src/main/java/com/mysite/minipage/diary/DiaryController.java
@@ -1,5 +1,6 @@
 package com.mysite.minipage.diary;
 
+import java.security.Principal;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -43,6 +45,41 @@ public class DiaryController {
 			return "diary_form";
 		}
 		this.diaryService.create(diaryForm.getContent());
+		
+		return "redirect:/diary/list";
+	}
+	
+	/* 글 수정 */
+	@GetMapping("/modify/{id}")
+	public String diaryModify(DiaryForm diaryForm, @PathVariable("id") Integer id) {
+		 Diary diary = this.diaryService.getDiary(id);
+		 
+		diaryForm.setContent(diary.getContent());
+		
+		return "diary_form";
+	 }
+	
+	@PostMapping("/modify/{id}")
+	public String diaryModify(@Valid DiaryForm diaryForm, BindingResult bindingResult,
+			@PathVariable("id") Integer id)
+	{
+		if(bindingResult.hasErrors()) {
+			return "diary_form";
+		}
+		
+		Diary diary = this.diaryService.getDiary(id);
+		
+		this.diaryService.modify(diary, diaryForm.getContent());
+		
+		return String.format("redirect:/diary/list/%s", id);
+	}
+	
+	/* 글 삭제 */
+	@GetMapping("/delete/{id}")
+	public String diaryDelete(Principal principal, @PathVariable("id") Integer id) {
+		Diary diary = this.diaryService.getDiary(id);
+		
+		this.diaryService.delete(diary);
 		
 		return "redirect:/diary/list";
 	}
--- minipage/src/main/java/com/mysite/minipage/diary/DiaryService.java
+++ minipage/src/main/java/com/mysite/minipage/diary/DiaryService.java
@@ -2,8 +2,11 @@ package com.mysite.minipage.diary;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+
+import com.mysite.minipage.DataNotFoundException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,6 +21,16 @@ public class DiaryService {
 		return this.diaryRepository.findAll();
 	}
 	
+	public Diary getDiary(Integer id) {
+		Optional<Diary> diary = this.diaryRepository.findById(id);
+		
+		if(diary.isPresent()) {
+			return diary.get();
+		} else {
+			throw new DataNotFoundException("Diary Not Found");
+		}
+	}
+	
 	// 글 등록
 	public void create(String content) {
 		Diary diary = new Diary();
@@ -26,5 +39,20 @@ public class DiaryService {
 		
 		this.diaryRepository.save(diary);
 	}
+	
+	// 글 수정
+	public void modify(Diary diary, String content) {
+		diary.setContent(content);
+		diary.setWriteDate(LocalDateTime.now());
+		
+		this.diaryRepository.save(diary);
+	}
+	
+	// 글 삭제
+	public void delete(Diary diary) {
+		this.diaryRepository.delete(diary);
+	}
+
+	
 	
 }
